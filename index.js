3450409cc635ba7e095caec610cb72196c8f6e73
Tjstevens19 <tdogstrike@gmail.com>
2023-11-30 18:20:04 -0700

    update port for deploy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let app = express();
 
 let path = require("path");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
